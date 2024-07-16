dd49dacb106a561a76dcf0ad2679c4375085bdfc
morill123 <166451139+morill123@users.noreply.github.com>
2024-07-16 15:01:06 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,65 +1,49 @@
-var map = L.map('map').setView([65.0, 26.0], 5);
-map.options.minZoom = -3;
+var map = L.map('map').setView([65.0, 26.0], 6);
 
-// Add OpenStreetMap background layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Fetch and display GeoJSON data
-fetch('https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326')
-    .then(response => response.json())
-    .then(data => {
-        var geojsonLayer = L.geoJSON(data, {
-            style: {
-                weight: 2
-            },
-            onEachFeature: onEachFeature
-        }).addTo(map);
-
-        map.fitBounds(geojsonLayer.getBounds());
-    });
-
-// Fetch migration data
-let positiveMigration = {};
-let negativeMigration = {};
+var positiveMigrationData, negativeMigrationData;
 
 fetch('https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f')
     .then(response => response.json())
     .then(data => {
-        data.dataset.dimension.Alue.category.label.forEach((name, key) => {
-            positiveMigration[name] = data.dataset.value[key];
-        });
-    });
-
-fetch('https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e')
+        positiveMigrationData = data.dataset.value;
+        return fetch('https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e');
+    })
     .then(response => response.json())
     .then(data => {
-        data.dataset.dimension.Alue.category.label.forEach((name, key) => {
-            negativeMigration[name] = data.dataset.value[key];
-        });
-    });
+        negativeMigrationData = data.dataset.value;
+        return fetch('https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326');
+    })
+    .then(response => response.json())
+    .then(data => {
+        var geojsonLayer = L.geoJson(data, {
+            style: function (feature) {
+                var positiveMigration = positiveMigrationData[feature.properties.kunta];
+                var negativeMigration = negativeMigrationData[feature.properties.kunta];
+                var hue = Math.min((positiveMigration / negativeMigration) * 60, 120);
+                return {
+                    color: `hsl(${hue}, 75%, 50%)`,
+                    weight: 2
+                };
+            },
+            onEachFeature: function (feature, layer) {
+                layer.bindTooltip(feature.properties.name);
+                layer.on('click', function () {
+                    var positiveMigration = positiveMigrationData[feature.properties.kunta];
+                    var negativeMigration = negativeMigrationData[feature.properties.kunta];
+                    var popupContent = `
+                        <b>${feature.properties.name}</b><br>
+                        Positive Migration: ${positiveMigration}<br>
+                        Negative Migration: ${negativeMigration}
+                    `;
+                    layer.bindPopup(popupContent).openPopup();
+                });
+            }
+        }).addTo(map);
 
-function onEachFeature(feature, layer) {
-    // Add tooltip
-    layer.bindTooltip(feature.properties.name);
-
-    // Add popup with migration data
-    layer.on('click', function() {
-        let positive = positiveMigration[feature.properties.name] || 0;
-        let negative = negativeMigration[feature.properties.name] || 0;
-        layer.bindPopup(`<strong>${feature.properties.name}</strong><br>Positive Migration: ${positive}<br>Negative Migration: ${negative}`).openPopup();
-    });
-
-    // Set style based on migration data
-    layer.setStyle({
-        color: getColor(feature.properties.name)
-    });
-}
-
-function getColor(name) {
-    let positive = positiveMigration[name] || 0;
-    let negative = negativeMigration[name] || 0;
-    let hue = Math.min((positive / negative) * 60, 120);
-    return `hsl(${hue}, 75%, 50%)`;
-}
+        map.fitBounds(geojsonLayer.getBounds());
+    })
+    .catch(error => console.error('Error fetching data:', error));
